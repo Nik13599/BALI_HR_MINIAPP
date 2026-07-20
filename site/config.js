d6b4d9b34eaa98de4cda04cc9a5b3443c1aba5b9
Nik13599 <KOLYASELEKH@gmail.com>
2026-07-21 00:40:34 +0300

    Add manager contact and social gift invoice settings

--- site/config.js
+++ site/config.js
@@ -4,10 +4,12 @@ window.BALI_CONFIG = {
   telegramUsername: "BALI_MINSK",
   telegramAuthEndpoint: "",
   vipInvoiceEndpoint: "",
+  socialGiftInvoiceEndpoint: "",
   venueName: "BALI Minsk",
   venuePhone: "+375296700300",
   venueAddress: "г. Минск, ул. Кирова, д. 13",
   telegramChannelUrl: "https://t.me/baliclubminsk",
+  managerTelegramUrl: "https://t.me/BALI_MINSK",
   instagramUrl: "https://www.instagram.com/baliminsk/",
   yandexMapUrl: "https://yandex.by/maps/org/bali_night_club/104137822369/"
 };
