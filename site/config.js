826585c5d2657bb60ded884449138a2643f5d8d0
Nik13599 <KOLYASELEKH@gmail.com>
2026-07-20 20:57:52 +0300

    chore: add Telegram CRM endpoint placeholder

--- site/config.js
+++ site/config.js
@@ -2,6 +2,7 @@ window.BALI_CONFIG = {
   supabaseUrl: "",
   supabaseAnonKey: "",
   telegramUsername: "BALI_MINSK",
+  telegramAuthEndpoint: "",
   venueName: "BALI Minsk",
   venuePhone: "+375296700300",
   venueAddress: "г. Минск, ул. Кирова, д. 13"
