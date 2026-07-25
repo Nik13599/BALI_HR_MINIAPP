86e5d75a9e65377311000eb9be99867527521eca
Nik13599 <KOLYASELEKH@gmail.com>
2026-07-25 21:51:54 +0300

    Remove Telegram and database configuration from web demo

--- site/config.js
+++ site/config.js
@@ -1,19 +1,13 @@
-window.BALI_CONFIG = {
+window.BALI_CONFIG = Object.freeze({
+  mode: "local-web-demo",
   demoOnly: true,
-  telegramAuthRequired: false,
-  supabaseUrl: "",
-  supabaseAnonKey: "",
-  telegramUsername: "BALI_MINSK",
-  telegramAuthEndpoint: "",
-  vipInvoiceEndpoint: "",
-  socialGiftInvoiceEndpoint: "",
-  checkinAppUrl: "./beta4-qr-app.html",
+  databaseEnabled: false,
+  externalAuthEnabled: false,
   venueName: "BALI Minsk",
   venuePhone: "+375296700300",
   venueAddress: "г. Минск, ул. Кирова, д. 13",
-  telegramChannelUrl: "https://t.me/baliclubminsk",
-  managerTelegramUrl: "https://t.me/BALI_MINSK",
+  managerContactUrl: "tel:+375296700300",
   instagramUrl: "https://www.instagram.com/baliminsk/",
   tiktokUrl: "https://www.tiktok.com/@baliminsk",
   yandexMapUrl: "https://yandex.by/maps/org/bali_night_club/104137822369/"
-};
+});
