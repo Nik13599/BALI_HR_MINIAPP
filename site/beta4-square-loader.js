9b31e2347d9835ab3595425bb452d70841331f9c
Nik13599 <KOLYASELEKH@gmail.com>
2026-07-26 03:56:38 +0300

    Release stable social links cache version

--- site/beta4-square-loader.js
+++ site/beta4-square-loader.js
@@ -1,5 +1,5 @@
 (async () => {
-  const version = "bali-full-demo-8-stable1";
+  const version = "bali-full-demo-8-stable2";
   window.BALI_DEMO_ONLY = true;
   window.BALI_WEB_DEMO = true;
   window.BALI_BROWSER_DEMO = true;
@@ -118,6 +118,7 @@
   }
 
   window.BaliWebDemoSanitize?.apply?.();
+  window.BaliHomeLinks?.sync?.();
   window.BaliFullDemoEvents?.decorateEvents?.();
   window.BaliFullDemoPeople?.mountCurrentEvent?.();
   window.BaliFullDemoPeopleFinalizer?.forceFullRender?.();
